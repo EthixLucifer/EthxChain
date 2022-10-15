26d16521d985e485efbdaab617b664d9dcb14ddd
Ethix Jambhulkar <60636670+EthixLucifer@users.noreply.github.com>
2022-10-15 12:59:04 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/1h_wkKfVo2skKqFaPLIkOZDD9-2IE7hI',
-      accounts: ['47aee86744a5947ac0290f7918b4910a45c2028c285c2cd66087fbf265c1c92c']
-    //   accounts: ['569ca17ffc0d0402dc44a8e0b54d1b88c23be8c1c5a2d583fde1c697c56bbb60']
+      accounts: ['account private key']
+    //   accounts: ['account private key']
     }
   }
 }
